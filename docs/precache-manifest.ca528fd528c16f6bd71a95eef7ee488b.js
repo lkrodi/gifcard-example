5da7c48142bb4547969361a78661ab23c87e0414
AlejandroLT192005 <alejandrox192005@gmail.com>
2020-08-12 23:38:13 -0500

    .

---
+++ docs/precache-manifest.ca528fd528c16f6bd71a95eef7ee488b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84c0651d70945099c43db7967e3729c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e001d7e16a161c1e551d",
+    "url": "/static/css/main.8b784831.chunk.css"
+  },
+  {
+    "revision": "55b0322e4e99f529880f",
+    "url": "/static/js/2.aa6f10eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa6f10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e001d7e16a161c1e551d",
+    "url": "/static/js/main.22ed9eba.chunk.js"
+  },
+  {
+    "revision": "81ac5a462e640c06803a",
+    "url": "/static/js/runtime-main.69e6fd44.js"
+  }
+]);
